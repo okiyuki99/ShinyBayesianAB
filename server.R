cce53ae4f677f63e4c4dbb6a88622ef3044507f7
gingi99 <bigtree3101@gmail.com>
2019-06-17 21:30:03 +0900

    [update] t.test

--- server.R
+++ server.R
@@ -5,42 +5,127 @@ shinyServer(function(input, output, session){
   values$AB <- NULL
   
   # --------------------------
-  # Display Lift from as_is to to_be
+  # UI
   output$ui_dlbtn <- renderUI({
     if(nrow(values$data) > 0){
       downloadButton("dl_data", "Download")
     }
   })
   
+  
+  output$ui_controll <- renderUI({
+    if(input$test_method == "prop.test"){
+      fluidRow(
+        column(3, numericInput('controll_trial', "# of trials", 250, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('controll_success', "# of success", 50, min = 0, max = Inf, step = 1))
+      )
+    }else if(input$test_method == "t.test"){
+      fluidRow(
+        column(3, numericInput('controll_trial', "# of trials", 250, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('controll_mean', "mean", 3, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('controll_sd', "sd", 1, min = 0, max = Inf, step = 1))
+      )
+    }
+  })
+  
+  output$ui_treatment <- renderUI({
+    if(input$test_method == "prop.test"){
+      fluidRow(
+        column(3, numericInput('treatment_trial', "# of trials", 250, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('treatment_success', "# of success", 60, min = 0, max = Inf, step = 1))
+      )
+    }else if(input$test_method == "t.test"){
+      fluidRow(
+        column(3, numericInput('treatment_trial', "# of trials", 250, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('treatment_mean', "mean", 3.3, min = 0, max = Inf, step = 1)),
+        column(3, numericInput('treatment_sd', "sd", 1.2, min = 0, max = Inf, step = 1))
+      )
+    }
+  })
+
+  output$ui_prior <- renderUI({
+    if(input$test_method == "prop.test"){
+      fluidRow(
+        column(6, numericInput('prior_alpha', "alpha", 20, step = 1)),
+        column(6, numericInput('prior_beta', "beta", 50, step = 1))
+      )
+    }else if(input$test_method == "t.test"){
+      fluidRow(
+        column(3, numericInput('prior_mu', "mu", 3, step = 1)),
+        column(3, numericInput('prior_lambda', "lambda", 1, step = 1)),
+        column(3, numericInput('prior_alpha', "alpha", 1, step = 1)),
+        column(3, numericInput('prior_beta', "beta", 1, step = 1))
+      )
+    }
+  })  
 
   # --------------------------
   # Create Data for table and plot
   observeEvent(input$btn_go, {
     
     # from input
-    c_trial <- input$controll_trial
-    c_success <- input$controll_success
-    t_trial <- input$test_trial
-    t_success <- input$test_success
-    alpha <- input$prior_alpha
-    beta <- input$prior_beta
-    n_simulation <- input$n_simulation
-    
-    c_binom <- rbinom(c_trial, 1, c_success / c_trial)
-    t_binom <- rbinom(t_trial, 1, t_success / t_trial)
+    if(input$test_method == "prop.test"){
+      c_trial <- input$controll_trial
+      c_success <- input$controll_success
+      t_trial <- input$treatment_trial
+      t_success <- input$treatment_success
+      alpha <- input$prior_alpha
+      beta <- input$prior_beta
+      n_simulation <- input$n_simulation
+      
+      c_binom <- rbinom(c_trial, 1, c_success / c_trial)
+      t_binom <- rbinom(t_trial, 1, t_success / t_trial)
+      
+      AB <- bayesTest(t_binom, c_binom,
+                      priors = c("alpha" = alpha, "beta" = beta),
+                      n_samples = n_simulation,
+                      distribution = "bernoulli")
+      
+      p_value <- fisher.test(matrix(c(c_trial, t_trial, c_success, t_success), nrow=2))$p.value
+      
+      work_data <- tibble(c_trial = c_trial, c_success = c_success, 
+                          t_trial = t_trial, t_success = t_success, 
+                          fisher_test_pvalue = p_value,
+                          prior_alpha = alpha, prior_beta = beta,
+                          n_simulation = n_simulation,
+                          win_prob = scales::percent(sum(AB$posteriors$Probability$A - AB$posteriors$Probability$B > 0) / n_simulation)
+      )
+      
+    }else if(input$test_method == "t.test"){
+      c_trial <- input$controll_trial
+      c_mean <- input$controll_mean
+      c_sd <- input$controll_sd
+      t_trial <- input$treatment_trial
+      t_mean <- input$treatment_mean
+      t_sd <- input$treatment_sd
+      
+      mu <- input$prior_mu
+      lambda <- input$prior_lambda
+      alpha <- input$prior_alpha
+      beta <- input$prior_beta
+      n_simulation <- input$n_simulation
+      
+      c_norm <- rnorm(c_trial, c_mean, c_sd)
+      t_norm <- rnorm(t_trial, t_mean, t_sd)
+      
+      AB <- bayesTest(t_norm, c_norm, 
+                      priors = c("mu" = mu, "lambda" = lambda, "alpha" = alpha, "beta" = beta), 
+                      n_samples = n_simulation, distribution = "normal")
+      
+      p_value <- t.test(t_norm, c_norm)$p.value
+      
+      work_data <- tibble(c_trial = c_trial, c_mean = c_mean, c_sd = c_sd,
+                          t_trial = t_trial, t_mean = t_mean, t_sd = t_sd,
+                          t_test_pvalue = p_value,
+                          prior_mu = mu, prior_lambda = lambda,
+                          prior_alpha = alpha, prior_beta = beta,
+                          n_simulation = n_simulation,
+                          win_prob = scales::percent(sum(AB$posteriors$Mu$A- AB$posteriors$Mu$B > 0) / n_simulation)
+      )
+    }
 
-    AB <- bayesTest(t_binom, c_binom,
-                    priors = c("alpha" = alpha, "beta" = beta),
-                    n_samples = n_simulation,
-                    distribution = "bernoulli")
-    values$AB <- AB
-    
     # save
-    work_data <- tibble(c_trial = c_trial, c_success = c_success, 
-                               t_trial = t_trial, t_success = t_success, 
-                               prior_alpha = alpha, prior_beta = beta,
-                               n_simulation = n_simulation,
-                               win_prob = scales::percent(sum(AB$posteriors$Probability$A - AB$posteriors$Probability$B > 0) / n_simulation))
+    values$AB <- AB
     
     # bind data
     values$data <- bind_rows(values$data, work_data) 
@@ -53,6 +138,9 @@ shinyServer(function(input, output, session){
   observeEvent(input$btn_remove,{
     if(nrow(values$data) > 0){
       values$data <- dplyr::slice(values$data, 1:nrow(values$data)-1)
+      if(nrow(values$data) == 0){
+        values$data <- tibble()
+      }
     }
   })
   
@@ -89,7 +177,19 @@ shinyServer(function(input, output, session){
   # Output : prior distribution
   # -----------------------------
   output$prior_distribution <- renderPlot({
-    bayesAB::plotBeta(input$prior_alpha, input$prior_beta) + ggtitle(paste0('alpha =',input$prior_alpha, ' beta = ', input$prior_beta))
+    if(input$test_method == "prop.test"){
+      shiny::req(input$prior_alpha)
+      shiny::req(input$prior_beta)
+      bayesAB::plotBeta(input$prior_alpha, input$prior_beta) + ggtitle(paste0('alpha =',input$prior_alpha, ' beta = ', input$prior_beta))
+    
+    }else if(input$test_method == "t.test"){
+      shiny::req(input$prior_mu)
+      shiny::req(input$prior_lambda)
+      shiny::req(input$prior_alpha)
+      shiny::req(input$prior_beta)
+      bayesAB::plotNormalInvGamma(input$prior_mu, input$prior_lambda, input$prior_alpha, input$prior_beta) + 
+        ggtitle(paste0('mu =',input$prior_mu, ' lambda = ', input$prior_lambda, ' alpha =',input$prior_alpha, ' beta = ', input$prior_beta))
+    }
   })
   
   #-----------------------------
@@ -97,7 +197,11 @@ shinyServer(function(input, output, session){
   # -----------------------------
   output$plot_posterior <- renderPlot({
     shiny::req(values$AB)
-    plot(values$AB)[[2]]$Probability
+    if(input$test_method == "prop.test"){
+      plot(values$AB)[[2]]$Probability
+    }else if(input$test_method == "t.test"){
+      plot(values$AB)[[2]]$Mu
+    }
   })
 
   #-----------------------------
@@ -105,7 +209,11 @@ shinyServer(function(input, output, session){
   # -----------------------------
   output$plot_probability <- renderPlot({
     shiny::req(values$AB)
-    plot(values$AB)[[3]]$Probability
+    if(input$test_method == "prop.test"){
+      plot(values$AB)[[3]]$Probability
+    }else if(input$test_method == "t.test"){
+      plot(values$AB)[[3]]$Mu
+    }
   })
 
 })
--- ui.R
+++ ui.R
@@ -15,44 +15,42 @@ shinyUI(
       tabItems(
         tabItem(tabName = "menu_top",
           fluidRow(
-            box(title = "Experiment", width = 6, solidHeader = F, status = "info", 
+            box(title = p(icon("cog"), "Experiment"), width = 6, solidHeader = F, status = "info", 
               fluidRow(
-                column(12, radioButtons('test_method', "Choose test", choices  = c("prop.test"), selected = "prop.test", inline = T))
+                column(12, radioButtons('test_method', "Choose test", choices  = c("prop.test","t.test"), selected = "prop.test", inline = T))
               ),
               fluidRow(
                 column(12, HTML("<hr>")),
-                column(12, HTML("<p><b>Controll Group</b></p>")),
-                column(3, numericInput('controll_trial', "# of trials", 250, min = 0, max = Inf, step = 1)),
-                column(3, numericInput('controll_success', "# of success", 50, min = 0, max = Inf, step = 1)),
-                column(12, HTML("<p><b>Treatment Group</b></p>")),
-                column(3, numericInput('test_trial', "Test Trials", 300, min = 0, max = Inf, step = 1)),
-                column(3, numericInput('test_success', "# of success", 70, min = 0, max = Inf, step = 1))
+                column(12, HTML("<p><b>Controll Group</b></p>"))
               ),
+              uiOutput("ui_controll"),
+              fluidRow(
+                column(12, HTML("<p><b>Treatment Group</b></p>"))
+              ),
+              uiOutput("ui_treatment"),
               fluidRow(
                 column(12, numericInput('n_simulation', "Simulations", 10000, min = 100, max = 10000000, step = 100)),
-                column(12, actionButton("btn_go", "Add Record"))
+                column(12, actionButton("btn_go", "Run"))
               )
             ),
-            box(title = "Prior", width = 6, solidHeader = F, status = "info", 
-                fluidRow(
-                  column(6, numericInput('prior_alpha', "Prior:alpha", 20, min = 2, max = 100, step = 1)),
-                  column(6, numericInput('prior_beta', "Prior:beta", 50, min = 2, max = 100, step = 1))
-                ),
+            box(title = p(icon("info-circle"), "Prior"), width = 6, solidHeader = F, status = "info", 
+                uiOutput("ui_prior"),
                 fluidRow(
                   column(12, plotOutput("prior_distribution") %>% withSpinner(type = 5))
                 )
             ),
             
-            box(title = "Result", width = 12, solidHeader = F, status = "success", 
+            box(title = p(icon("area-chart"),"Result"), width = 12, solidHeader = F, status = "success", 
               fluidRow(
                 column(6, plotOutput("plot_posterior") %>% withSpinner(type = 5)),
                 column(6, plotOutput("plot_probability") %>% withSpinner(type = 5))
-              ),
+              )
+            ),
+            box(title = p(icon("history"),"History"), width = 12, solidHeader = F, status = "success", 
               fluidRow(
                 column(12, tableOutput("kable_proportion")),
                 column(1, actionButton("btn_remove", "Remove Record")),
-                column(1, uiOutput("ui_dlbtn")),
-                column(10, uiOutput("ui_unvisible_columns"))
+                column(1, uiOutput("ui_dlbtn"))
               )
             )
           )
